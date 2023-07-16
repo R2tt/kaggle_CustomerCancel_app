77a2f7fc4294042a22be4a7c4ecaf693c44b75f3
R2tt <youheip19hasegawadm4@gmail.com>
2023-07-16 20:03:43 +0900

    画面から著作権に抵触するロゴや文言削除＋requirements.txtファイルの経過ファイルを保存

--- customerCancel.py
+++ customerCancel.py
@@ -1,13 +1,3 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html')
-# 
-# if __name__ == "__main__":
-#     app.run()
-
 import os
 from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
@@ -43,17 +33,12 @@ def upload_file():
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
-            #受け取ったファイルを読み込み、np形式に変換
-            # img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
-            # img = image.img_to_array(img)
-            # data = np.array([img])
+            #受け取ったファイルを読み込む
             data = pd.read_csv(filepath)
             #変換したデータをモデルに渡して予測する
             data["product_prd_1"] = [1 if s=="prd_1" else 0 for s in data["product"]]
             data["product_prd_2"] = [1 if s=="prd_2" else 0 for s in data["product"]]
             result = model.predict_proba(data[["age","usage_period","product_prd_1","product_prd_2"]])
-            #predicted = result.argmax()
-            #pred_answer = classes[predicted]
             return render_template("index.html",answer=result[:,1])
 
     return render_template("index.html",answer="")
